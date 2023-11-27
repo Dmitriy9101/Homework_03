b5d4b385e67bc8b308d06e906c5deaf35b2e1e7b
Dmitriy <dmitriy.chicunov@gmail.com>
2023-11-28 01:50:00 +0300

    HomeWork_03

---
+++ Seminar_03/HomeWork_03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100
+// Найдите количество элементов массива, значения которых лежат
+// в отрезке [20,90]
+int size = 10;
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = new Random().Next(1, 101); 
+}
+Console.WriteLine($"[{string.Join("; ",array)}]");
