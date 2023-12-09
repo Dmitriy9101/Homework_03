2c4182b1037d60cb9bd368e64b121e4c5178abac
Dmitriy9101 <dmitriy.chicunov@gmail.com>
2023-12-10 01:21:01 +0300

    Vau

--- Seminar_06/Program.cs
+++ Seminar_06/Program.cs
@@ -15,13 +15,7 @@
 //     for (int i = 0; i < 5; i++)
 //     {
 //         for (int j = 0; j < 5; j++)
-//         {
-
-//         }
-//     }
-
 // }
-
 
 // char[,] array = { "H", "E", "L", "L", "O" };
 // string result = CharArray(chars);
