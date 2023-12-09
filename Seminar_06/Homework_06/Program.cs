3e8ebbcc3fd6aa824f470394cfb062614f99d719
Dmitriy9101 <dmitriy.chicunov@gmail.com>
2023-12-10 01:18:23 +0300

    Vau

---
+++ Seminar_06/Homework_06/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+// char[,] CreateMatrix(char rows, char colums, char min, char max)
+// {
+//     string[,] matrix = new string[rows, colums];
+//     // Матрица размером rows на colums
+//     Random rand = new Random();
+//     for (char i = 0; i < rows; i++) //i < matrix.GetLength(0), строчки
+//     {
+//         // i, j, m, k
+//         for (char j = 0; j < colums; j++) // столбцы j < matrix.GetLength(1)
+//         {
+//             i = Convert.ToInt16();
+//             matrix[i, j] = rand.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// Задача 1: Задайте двумерный массив символов (тип char [,]).
+//  Создать строку из символов этого массива.
+
+// Console.WriteLine("Введите размеры массива ");
+// // string i = Console.ReadLine();
+// // string j = Console.ReadLine();
+
+
+// char[,] = m (char i, char j, char min, char max)
+// {}
+// char*** create(int num){
+//     char *** temp = (char ***) malloc(num * sizeof(char **));
+//     for (int i=0; i < num; i++) {
+//         temp[i] = (char **) malloc(num * sizeof(char*));
+//         for (int j = 1; j < num; j++)
+//             temp[i][j] = (char *) malloc(num * sizeof(char*));   
+//     }
+//     return temp;
+// }
---
+++ Seminar_06/Program.cs
@@ -0,0 +1,43 @@
+﻿// int[] myArray = { 1, 3, 5, 6 };
+// "1" + "2" = "12"
+// indexes = [row,column]
+
+// cтрочка- массив символов, символ char
+// [a b c d] = > abcd
+
+// using System.Drawing;
+// using System.Linq.Expressions;
+// using System.Reflection.Metadata.Ecma335;
+
+// string CharArray(char[,] array);
+// {
+//     string result = String.Empty; // указываем пустую строчку ""
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+
+//         }
+//     }
+
+// }
+
+
+// char[,] array = { "H", "E", "L", "L", "O" };
+// string result = CharArray(chars);
+// Console.WriteLine($"Result: {result}");
+
+int[,] myArr = new int[4, 5];
+
+Random ran = new Random();
+
+// Инициализируем данный массив
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+       myArr[i, j] = ran.Next(1, 15);
+       Console.Write("{0}\t", myArr[i, j]);
+    }
+    Console.WriteLine();
+}
